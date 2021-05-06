203c21af6c63e4d61a7efbca4c54f7087a90b783
xJuhix <49311688+xJuhix@users.noreply.github.com>
2021-05-06 17:35:36 +0200

    Update api.js

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "https://api.rawg.io/api/games?key=122ef77380eb492fb3157c564a0e73b9"
+export const BASE_URL = "https://api.rawg.io/api/games"
+export const KEY = "?key=122ef77380eb492fb3157c564a0e73b9"
